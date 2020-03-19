0707e707bbdff5f293b0082e9765cd8fba37a465
JesrelleA <jsabenojar@up.edu.ph>
2020-03-19 23:02:56 +0800

    'working'

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # CS199_ThesisProject
+
+Helpful links for creating Menu
+> https://youtu.be/kEyE1uh6xUo?t=3
---
+++ VRGame/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+/*
+Scene names and index
+    MainMenu_Scene      0
+    CreatePlayer_Scene  1
+    ViewPlayer_Scene    2
+    OpeningGame_Scene   3
+    Game_Scene          4
+*/
+
+public class MainMenu : MonoBehaviour
+{
+    public void ViewPlayers() {
+        SceneManager.LoadScene(2);
+    }
+
+    /*
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    */
+}
