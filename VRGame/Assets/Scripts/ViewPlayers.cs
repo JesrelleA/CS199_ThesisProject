8d10ce406a5a5bc55c10a734e21744a8e9555036
JesrelleA <jsabenojar@up.edu.ph>
2020-03-24 21:02:05 +0800

    new script file for view players

---
+++ VRGame/Assets/Scripts/ViewPlayers.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ViewPlayers : MonoBehaviour
+{
+
+
+    public void Home() {
+        SceneManager.LoadScene(0);
+    }
+
+    /*
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    */
+}
