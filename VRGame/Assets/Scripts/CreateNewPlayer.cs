9e65be40afe52671095f145a392930bb8ad01542
JesrelleA <jsabenojar@up.edu.ph>
2020-05-06 23:57:16 +0800

    updated files: working insert

--- VRGame/Assets/Scripts/CreateNewPlayer.cs
+++ VRGame/Assets/Scripts/CreateNewPlayer.cs
@@ -3,13 +3,85 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class CreateNewPlayer : MonoBehaviour
 {
     public InputField firstname;
     public InputField lastname;
     public InputField age;
-    public InputField sex;
+    //public InputField sex;
+    public Toggle male;
+    public Toggle female;
+
+    public InputField remarks;
+
+    public void Start() {
+
+    }
+
+    /*
+    public void CreatePlayer() {
+        string conn = "URI=file:" + Application.dataPath + "/gamedb.s3db;"; //Path to database
+
+        Debug.Log(conn);
+        IDbConnection dbconn;
+        dbconn = (IDbConnection)new SqliteConnection(conn);
+        Debug.Log("new sqlite conn");
+        dbconn.Open(); //Open connection to database
+        Debug.Log("db open");
+
+        IDbCommand dbcmd = dbconn.CreateCommand();
+
+        string sqlQuery = "SELECT * FROM Players;"; //"INSERT INTO Players(Name, Age, Sex, Remarks) VALUES(\"hello2\", 6, \"f\", \"WORKED YAAAY\");"; //"SELECT * FROM Players;"; //"SELECT Name, Age, Sex, Remarks FROM Players";
+
+        dbcmd.CommandText = sqlQuery;
+        Debug.Log(sqlQuery);
+        IDataReader /*var*/ //reader = dbcmd.ExecuteReader();
+
+        //Debug.Log("HWEERER");
+
+        
+        /*INSERT TO PLAYERS*/
+        //INSERT INTO Players(Name, Age, Sex, Remarks) 
+        //VALUES
+
+        /*INSERT TO RECORDS*/
+        //INSERT INTO Records (PlayerID, Record, DateRecorded)
+        //VALUES (
+        //(SELECT PlayerID FROM Players ORDER BY PlayerID DESC LIMIT 1),
+        //"No record",
+        //Date('now')
+        //);
+
+        /*
+        string playername = "sadsfasfd";
+        int age1 = 1;
+        string playersex = "f";
+        string remarks1 = "sdfg";
+    
+        while (reader.Read()) {
+            Debug.Log("inside while 1");
+            playername = Convert.ToString(reader[1]);//.GetString(1);
+            age1 = Convert.ToInt32(reader[2]);//.GetInt32(0);
+            playersex = Convert.ToString(reader[3]);//.GetString(1);
+            remarks1 = Convert.ToString(reader[4]);//.GetString(1);
+            Debug.Log("Name: " + playername + " Age: " + age1 + " Sex: " + playersex + " Remarks: " + remarks1);
+            Debug.Log("inside while 2");
+        }
+        */
+        /*
+        Debug.Log("before reader close");
+        reader.Close();
+        reader = null;
+        dbcmd.Dispose();
+        dbcmd = null;
+        dbconn.Close();
+        dbconn = null;
+        Debug.Log("Fin yaay");
+        
+    }
+    */
 
     public void Home() {
         SceneManager.LoadScene(0);
--- VRGame/Assets/Scripts/Database1.cs
+++ VRGame/Assets/Scripts/Database1.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 using Mono.Data.Sqlite;
 using System.Data;
@@ -8,14 +9,31 @@ using System;
 
 public class Database1 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    /*
+    private string firstname;
+    private string lastname;
+    private string fullname; // = firstname + " " + lastname
+    private int age;
+    private string sex;
+    private string remarks;
+
+    public GameObject fname1;
+    public InputField fnameobj;
+    public GameObject lnameobj;
+    public GameObject ageobj;
+    public GameObject sexobj;
+    public GameObject remarksobj;
+    */
+
     void Start()
     {
-        string conn = "URI=file:" + Application.dataPath + "/gamedb.s3db;"; //"/db1.s3db"; //Path to database
+        
+        
+       
+        
+        string conn = "URI=file:" + Application.dataPath + "/gamedb.s3db;"; //Path to database
 
-        //Debug.Log("hi?");
         Debug.Log(conn);
-        //Debug.Log("at least??");
         IDbConnection dbconn;
         dbconn = (IDbConnection)new SqliteConnection(conn);
         Debug.Log("new sqlite conn");
@@ -24,32 +42,36 @@ public class Database1 : MonoBehaviour
 
         IDbCommand dbcmd = dbconn.CreateCommand();
 
-        if (dbcmd!= null)
-        {
-            Debug.Log("IN");
-            Debug.Log(dbcmd);
-        } else {
-            Debug.Log("NOPE :(");
-        }
-
-        string sqlQuery = "SELECT * FROM Players;"; //"INSERT INTO Players(Name, Age, Sex, Remarks) VALUES(\"hello2\", 6, \"f\", \"WORKED YAAAY\");"; //"SELECT * FROM Players;"; //"SELECT Name, Age, Sex, Remarks FROM Players";
+        string sqlQuery = "INSERT INTO Players(Name, Age, Sex, Remarks) VALUES(\"hello4\", 7, \"m\", \"WzzY\");"; //"SELECT * FROM Players;"; //"SELECT Name, Age, Sex, Remarks FROM Players";
 
         dbcmd.CommandText = sqlQuery;
         Debug.Log(sqlQuery);
         IDataReader /*var*/ reader = dbcmd.ExecuteReader();
 
-        Debug.Log("HWEERER");
+        //Debug.Log("HWEERER");
 
         
+        /*INSERT TO PLAYERS*/
+        //INSERT INTO Players(Name, Age, Sex, Remarks) 
+        //VALUES
+
+        /* //INSERT TO RECORDS
+        INSERT INTO Records (PlayerID, Record, DateRecorded)
+        VALUES (
+        (SELECT PlayerID FROM Players ORDER BY PlayerID DESC LIMIT 1),
+        "No record",
+        Date('now')
+        );
+        */
+
+        /*
         string playername = "sadsfasfd";
         int age1 = 1;
         string playersex = "f";
         string remarks1 = "sdfg";
     
-        
         while (reader.Read()) {
             Debug.Log("inside while 1");
-            //Debug.Log(String.Format("{0}", reader[1]));
             playername = Convert.ToString(reader[1]);//.GetString(1);
             age1 = Convert.ToInt32(reader[2]);//.GetInt32(0);
             playersex = Convert.ToString(reader[3]);//.GetString(1);
@@ -57,18 +79,40 @@ public class Database1 : MonoBehaviour
             Debug.Log("Name: " + playername + " Age: " + age1 + " Sex: " + playersex + " Remarks: " + remarks1);
             Debug.Log("inside while 2");
         }
+        */
         
-        //Debug.Log("name=" + playername + " age=" + age1 + " sex=" + playersex + " remarks=" + remarks1);
-        
-
         Debug.Log("before reader close");
         reader.Close();
         reader = null;
+
+
+        string sqlQuery2 = "INSERT INTO Records (PlayerID, Record, DateRecorded) VALUES ((SELECT PlayerID FROM Players ORDER BY PlayerID DESC LIMIT 1), \"No record\", Date('now'));";
+        dbcmd.CommandText = sqlQuery2;
+        Debug.Log(sqlQuery2);
+        IDataReader /*var*/ reader2 = dbcmd.ExecuteReader();
+
+        /*int id = 0;
+
+        while (reader2.Read()) {
+            Debug.Log("inside while2");
+            id = Convert.ToInt32(reader[0]);
+            Debug.Log(id);
+        }*/
+
+        Debug.Log("before reader2 close");
+        reader2.Close();
+        Debug.Log("after reader2 close");
+        reader2 = null;
+        Debug.Log("after null");
+
+
+
         dbcmd.Dispose();
         dbcmd = null;
         dbconn.Close();
         dbconn = null;
         Debug.Log("Fin yaay");
+        
     }
 
     // Update is called once per frame
